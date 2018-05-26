64db6908732315514ae77478a853f287ab643e4c
lightningroood <lightningroood@LAPTOP-LREMCV09.localdomain>
2018-05-27 02:22:47 +0800

    corrected typo

--- init.el
+++ init.el
@@ -12,9 +12,9 @@
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
-(global-set-key (kbd "M-x") #'heml-M-x)
-(global-set-key (kbd "C-x r b") #'heml-filtered-bookmarks)
-(global-set-key (kbd "C-x C-f") #'heml-find-files)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
 (helm-mode 1)
 
 
