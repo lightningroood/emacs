b8d5cde5495390ddbbcaf4131b8d40c40adf2c62
lightningroood <lightningroood@LAPTOP-LREMCV09.localdomain>
2018-05-27 02:00:13 +0800

    helm-projectile magit

---
+++ init.el
@@ -0,0 +1,20 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+(projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+(global-set-key (kbd "M-x") #'hlem-M-x)
+(global-set-key (kbd "C-x r b") #'hlem-filtered-bookmarks)
+(global-set-key (kbd "C-c C-f") #'hlem-find-files)
+(helm-mode 1)
+
+
